a029a1a76356ec06b70f6772878461a28c5ae970
Kenny D <alphakex2@gmail.com>
2020-04-05 20:20:32 +0300

    Added a Shop Page collection preview and collection Item

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/homepage/shop/shop.component';
 
 function App() {
   return (
     <div >
       <Switch>
       <Route exact path="/" component={Homepage} />
+      <Route exact path="/shop" component={ShopPage} />
         {/* <Route exact path="/hats" component={HatsPage} /> */}
       </Switch>
     </div>
---
+++ src/pages/homepage/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+import StoreData from "./shop-data"
+import CollectionPreview from './../../../components/collection-preview/collection-preview.component';
+const ShopPage = () => {
+  const [collections] = useState(StoreData)
+  return (
+    <div className="shop-page">
+      {collections.map(({id, ...otherCollectionProps}) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  )
+}
+
+export default ShopPage
